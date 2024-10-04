32570a1392a162a1e9a582eec769693925144aee
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-10-04 14:35:41 +0200

    RK2

--- Potentiel.py
+++ Potentiel.py
@@ -5,9 +5,9 @@ from math import*
 def Kepler(x, y):
     return -1/sqrt(x**2 + y**2)
 
-def Fx (Pot, x, y, h):
+def Fx (Pot, x, y, h= 1e-2):
     return (Pot(x+h, y) - Pot(x, y)) / h
 
 
-def Fy (Pot, x, y, h):
+def Fy (Pot, x, y, h = 1e-2):
     return (Pot(x,y+h) - Pot(x,y)) / h
